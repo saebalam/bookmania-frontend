81092fe5243d41229b9c55add9df35d160514012
saebalam <saebalam123@gmail.com>
2022-08-24 14:04:57 +0530

    second commit

---
+++ build/precache-manifest.61393e62cad964ad09e8e96276b1ded1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26693347cb5bcc4d34f7e9a236843db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a21819c9e83632710021",
+    "url": "/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "689a515c1b077b2c37f3",
+    "url": "/static/css/main.1ee9843a.chunk.css"
+  },
+  {
+    "revision": "a21819c9e83632710021",
+    "url": "/static/js/2.1219e3a1.chunk.js"
+  },
+  {
+    "revision": "689a515c1b077b2c37f3",
+    "url": "/static/js/main.32096480.chunk.js"
+  },
+  {
+    "revision": "8ab6be022549c338d532",
+    "url": "/static/js/runtime~main.5f0d2525.js"
+  },
+  {
+    "revision": "1bb7986beae5e1279dfe064a484d5ef0",
+    "url": "/static/media/card.1bb7986b.jpg"
+  },
+  {
+    "revision": "7c345ed21af7cfc834c9f4a6f211072b",
+    "url": "/static/media/card2.7c345ed2.jpg"
+  },
+  {
+    "revision": "cbe1fe2242d520981a29f0ba219bfdbc",
+    "url": "/static/media/card3.cbe1fe22.jpg"
+  },
+  {
+    "revision": "3d58f99bd19dc015f018bc6548a4229d",
+    "url": "/static/media/card4.3d58f99b.jpg"
+  },
+  {
+    "revision": "234b945f076326e58bdaac683c122c45",
+    "url": "/static/media/card5.234b945f.jpg"
+  },
+  {
+    "revision": "b2bb780c246176f61fd2075af5d366b5",
+    "url": "/static/media/card6.b2bb780c.jpg"
+  },
+  {
+    "revision": "d0c2b832b3a6d7eff444c958fb75011a",
+    "url": "/static/media/card7.d0c2b832.jpg"
+  },
+  {
+    "revision": "f339e25cb72065eece2826c3bcc33c26",
+    "url": "/static/media/carousel1.f339e25c.jpg"
+  },
+  {
+    "revision": "2fa8f9b70e56a620e09b582bdac354ac",
+    "url": "/static/media/carousel2.2fa8f9b7.jpg"
+  },
+  {
+    "revision": "29eecdff352162beb9def43b4621704c",
+    "url": "/static/media/carousel4.29eecdff.jpg"
+  },
+  {
+    "revision": "193925939019863c438e998b7d904975",
+    "url": "/static/media/empty_cart2.19392593.png"
+  },
+  {
+    "revision": "a4f53cf5d64a53edfc10bc4eed1eb0bd",
+    "url": "/static/media/empty_wishlist.a4f53cf5.png"
+  },
+  {
+    "revision": "a6e196669e2541f0a7d468d1e685d9b8",
+    "url": "/static/media/form.a6e19666.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
